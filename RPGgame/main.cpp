1569c71dc9146cdacac98fca8e785bdb519119a8
pyke <pykelysia0206@163.com>
2025-06-23 10:48:03 +0800

    添加项目文件。

---
+++ RPGgame/main.cpp
@@ -0,0 +1,19 @@
+#include<graphics.h>
+#include "namespace.h"
+
+int main() {
+
+	initgraph(1280, 640);
+
+	while (true) {
+		mystd::TIME start = GetTickCount();
+
+		mystd::TIME end = GetTickCount();
+		auto delta = end - start;
+		if()
+	}
+
+	closegraph();
+
+	return 0;
+}
---
+++ RPGgame/namespace.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace mystd {
+	typedef unsigned long long ull;
+	typedef ull TIME;
+}
